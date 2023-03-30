790445cd36ca3346b2a2c71e29cd820951e53122
mrayandika15 <mrayandika15@gmail.com>
2023-03-30 21:28:05 +0700

    fix(): deployment issue

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,14 +1,3 @@
-import useScrollDirection from "./useScrollDirection";
-
 import useDebounce from "./useDebounce";
 
-import useScrollPosition from "./useScrollPosition";
-
-import useScrollAnimation from "./useScrollAnimation";
-
-export {
-  useScrollDirection,
-  useDebounce,
-  useScrollPosition,
-  useScrollAnimation,
-};
+export { useDebounce };
