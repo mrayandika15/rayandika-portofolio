3375b3c5cce750b2d146f561d409032a01446256
mrayandika15 <mrayandika15@gmail.com>
2024-11-09 17:13:41 +0700

    BREAKING-CHANGES: clean projects

--- components/core/Navbar/Navbar.type.ts
+++
@@ -1,3 +0,0 @@
-export type INavbar = {
-  fullPageScroll: boolean;
-};
--- components/icons/IcMouseWheel.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./MouseWheel.module.css";
-
-const IcMouseWheel = () => {
-  return <div className={style.mouse}></div>;
-};
-
-export default IcMouseWheel;
